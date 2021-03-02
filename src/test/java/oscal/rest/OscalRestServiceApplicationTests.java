383275545f4bf23f1ea8d1be9ed2dc53a9d93ba8
zclarkED <zclark@easydynamics.com>
2021-03-02 06:49:12 -0500

    create placeholder for tests

---
+++ src/test/java/oscal/rest/OscalRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package oscal.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OscalRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
