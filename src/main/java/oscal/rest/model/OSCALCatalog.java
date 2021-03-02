86232f9790debc10ce8fe07145899f99408bd87a
zclarkED <zclark@easydynamics.com>
2021-03-02 06:43:01 -0500

    create OSCALCatalog dummy model for demo, will use liboscal for actual implementation

---
+++ src/main/java/oscal/rest/model/OSCALCatalog.java
@@ -0,0 +1,18 @@
+package oscal.rest.model;
+
+public class OSCALCatalog {
+
+    private String catalogId;
+
+    private String name;
+
+
+    public String getId() {
+        return catalogId;
+    }
+    public void setCatalogId(String catalogId) {this.catalogId = catalogId;}
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
