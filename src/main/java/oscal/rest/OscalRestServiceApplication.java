9afb480cda997bfb7032869120c5454a88807f85
zclarkED <zclark@easydynamics.com>
2021-03-02 06:40:27 -0500

    create application to run the API

---
+++ src/main/java/oscal/rest/OscalRestServiceApplication.java
@@ -0,0 +1,13 @@
+package oscal.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OscalRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OscalRestServiceApplication.class, args);
+	}
+
+}
