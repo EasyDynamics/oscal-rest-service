293a75e3ee75b510a2ee9c912a4107be0af9619a
zclarkED <zclark@easydynamics.com>
2021-03-02 06:39:25 -0500

    create SwaggerDocumentationConfig.java, used to add information to Swagger UI

---
+++ src/main/java/oscal/rest/configuration/SwaggerDocumentationConfig.java
@@ -0,0 +1,64 @@
+package oscal.rest.configuration;
+/*
+public class SwaggerDocumentationConfig {
+}
+*/
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+
+@Configuration
+public class SwaggerDocumentationConfig {
+
+    @Bean
+    public Docket customImplementation(){
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("oscal.rest"))
+                .build()
+
+                //.directModelSubstitute(org.threeten.bp.LocalDate.class, java.sql.Date.class)
+                //.directModelSubstitute(org.threeten.bp.OffsetDateTime.class, java.util.Date.class)
+                .apiInfo(apiInfo());
+    }
+
+    ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("OSCAL REST")
+                .description("REST API specification for interacting with OSCAL models.")
+                .license("")
+                .licenseUrl("http://unlicense.org")
+                .termsOfServiceUrl("")
+                .version("1.0.0")
+                .contact(new Contact("","", "rgauss@easydynamics.com"))
+                .build();
+    }
+
+    @Bean
+    public OpenAPI openApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("OSCAL REST")
+                        .description("REST API specification for interacting with OSCAL models.")
+                        .termsOfService("")
+                        .version("1.0.0")
+                        .license(new License()
+                                .name("")
+                                .url("http://unlicense.org"))
+                        .contact(new io.swagger.v3.oas.models.info.Contact()
+                                .email("rgauss@easydynamics.com")));
+    }
+
+}
