79d20dbcc2e65df9c464116fc7eff4eae6a9e384
zclarkED <zclark@easydynamics.com>
2021-03-04 01:25:39 -0500

    annotate OSCALCatalog model for testing

--- src/main/java/io/swagger/model/OSCALCatalog.java
+++ src/main/java/io/swagger/model/OSCALCatalog.java
@@ -1,6 +1,12 @@
 package io.swagger.model;
 
+import java.util.Date;
+import java.util.Map;
 import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.validation.annotation.Validated;
 import javax.validation.Valid;
 import javax.validation.constraints.*;
@@ -9,14 +15,37 @@ import javax.validation.constraints.*;
  * OSCALCatalog
  */
 @Validated
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2021-02-22T21:37:05.909Z[GMT]")
+//@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2021-02-22T21:37:05.909Z[GMT]")
 
-
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class OSCALCatalog   {
 
-  private String catalogId;
-  private String name;
+  private String uuid;
+  private String title;
 
+  public String getUuid() {
+    return uuid;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  @Override
+  public String toString() {
+    return "OSCALCatalog{" +
+            "uuid='" + uuid + '\'' +
+            ", title=" + title +
+            '}';
+  }
+  @SuppressWarnings("unchecked")
+  @JsonProperty("metadata")
+  private void unpackNested(Map<String,Object> metadata) {
+    this.title = (String)metadata.get("title");
+
+  }
+
+/*
   @Override
   public boolean equals(java.lang.Object o) {
     if (this == o) {
@@ -42,14 +71,23 @@ public class OSCALCatalog   {
     return sb.toString();
   }
 
-  /**
-   * Convert the given object to string with each line indented by 4 spaces
-   * (except the first line).
-   */
+
   private String toIndentedString(java.lang.Object o) {
     if (o == null) {
       return "null";
     }
     return o.toString().replace("\n", "\n    ");
   }
+
+ */
 }
+/*
+public class Metadata{
+  public String title;
+  public Date published;
+  @JsonProperty("last-modified")
+  public Date lastModified;
+  public String version;
+  @JsonProperty("oscal-version")
+  public String oscalVersion;
+}*/
