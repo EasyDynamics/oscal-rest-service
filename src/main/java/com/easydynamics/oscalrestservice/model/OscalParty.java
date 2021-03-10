eddce17d64ad719be07744928b30c4b22b42d066
zclarkED <zclark@easydynamics.com>
2021-03-10 17:59:56 -0500

    Create OscalParty.java model
    This model will be used to create objects for the database.
    The 3 variables (uuid, type, and name) have been selected based on @rgauss
    decision for the /parties endpoint.
    uuid is automatically generated, and is in uuidv4 format.

---
+++ src/main/java/com/easydynamics/oscalrestservice/model/OscalParty.java
@@ -0,0 +1,26 @@
+package com.easydynamics.oscalrestservice.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "parties")
+@Data
+public class OscalParty {
+
+  @Id
+  @GeneratedValue(generator = "uuid2")
+  @GenericGenerator(name = "uuid2", strategy = "uuid2")
+  @Column(length = 36)
+  private String uuid;
+
+  private String type;
+
+  private String name;
+
+}
