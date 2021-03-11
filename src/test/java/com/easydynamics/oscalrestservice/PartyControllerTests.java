bca23655c06024ceb8682e77e2d3b533f7006d43
zclarkED <zclark@easydynamics.com>
2021-03-11 13:24:48 -0500

    Create tests for findAllParties GET Request
    - test creates a new object to get, and ensures that status 200 is
    returned

--- src/test/java/com/easydynamics/oscalrestservice/PartyControllerTests.java
+++ src/test/java/com/easydynamics/oscalrestservice/PartyControllerTests.java
@@ -9,6 +9,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 //import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.easydynamics.oscalrestservice.model.OscalParty;
+import com.easydynamics.oscalrestservice.repository.PartyRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -23,6 +25,21 @@ public class PartyControllerTests {
   @Autowired
   private MockMvc mockMvc;
 
+  @Autowired
+  private PartyRepository testRepository;
 
+  /**
+   * Test to see if the POST Request to /parties will be valid when provided a good request
+   * @throws Exception
+   */
+
+  @Test
+  public void getRequestTest() throws Exception {
+    OscalParty party = new OscalParty("organization", "IT Department");
+    testRepository.save(party);
+    this.mockMvc.perform(get("/oscal/v1/parties"))
+        .andDo(print())
+        .andExpect(status().isOk());
+  }
 
 }
