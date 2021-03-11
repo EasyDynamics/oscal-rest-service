6bc8e403436242b707c7ec0b733bdf94a757d46c
zclarkED <zclark@easydynamics.com>
2021-03-11 11:10:55 -0500

    remove GET/POST requests for separate PRs

--- src/test/java/com/easydynamics/oscalrestservice/PartyControllerTests.java
+++ src/test/java/com/easydynamics/oscalrestservice/PartyControllerTests.java
@@ -1,5 +1,7 @@
 package com.easydynamics.oscalrestservice;
 
+import static com.easydynamics.oscalrestservice.api.CatalogController.CATALOG_ID_80053r5;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -21,34 +23,6 @@ public class PartyControllerTests {
   @Autowired
   private MockMvc mockMvc;
 
-  /**
-   * Test to see if the POST Request to /parties will be valid when provided a good request
-   * @throws Exception
-   */
 
-  @Test
-  public void giveValidRequest_thenVerifyResponse() throws Exception {
-    this.mockMvc.perform(post("/oscal/v1/parties")
-        .contentType(MediaType.APPLICATION_JSON)
-        .content("{\"type\": \"bob\", \"name\" : \"the name\"}"))
-        .andDo(print())
-        .andExpect(status().isOk())
-        .andExpect(content().contentType("application/json"))
-        .andExpect(jsonPath("$.type").value("bob"));
-  }
-
-  /**
-   * Test to see if the POST Request to /parties will be invalid when provided a bad request
-   * @throws Exception
-   */
-
-  @Test
-  public void giveInvalidRequest_thenVerifyResponse() throws Exception {
-    this.mockMvc.perform(post("/oscal/v1/parties")
-        .contentType(MediaType.APPLICATION_JSON)
-        .content("{ \"name\" : \"the name\"}"))
-        .andDo(print())
-        .andExpect(status().isBadRequest());
-  }
 
 }
