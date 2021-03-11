147a2fb77bd826cafe4f0983a823e11b705444f3
zclarkED <zclark@easydynamics.com>
2021-03-10 22:53:23 -0500

    Optimize imports

--- src/main/java/com/easydynamics/oscalrestservice/api/PartyController.java
+++ src/main/java/com/easydynamics/oscalrestservice/api/PartyController.java
@@ -1,17 +1,8 @@
 package com.easydynamics.oscalrestservice.api;
 
 
-import com.easydynamics.oscalrestservice.model.OscalParty;
 import com.easydynamics.oscalrestservice.repository.PartyRepository;
-import io.swagger.v3.oas.annotations.Parameter;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
