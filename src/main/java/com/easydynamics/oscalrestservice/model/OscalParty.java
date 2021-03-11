9b1c1984026644c79d2e2d91cabd7d7831cc73eb
zclarkED <zclark@easydynamics.com>
2021-03-10 22:39:34 -0500

    add @NotBlank to type and name in OscalParty

--- src/main/java/com/easydynamics/oscalrestservice/model/OscalParty.java
+++ src/main/java/com/easydynamics/oscalrestservice/model/OscalParty.java
@@ -5,6 +5,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import lombok.Data;
 import org.hibernate.annotations.GenericGenerator;
 
@@ -19,8 +22,10 @@ public class OscalParty {
   @Column(length = 36)
   private String uuid;
 
+  @NotBlank(message = "Type cannot be null")
   private String type;
 
+  @NotBlank(message = "Name cannot be null")
   private String name;
 
 }
