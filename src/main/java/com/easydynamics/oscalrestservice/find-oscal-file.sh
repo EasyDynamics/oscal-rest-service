0e25be67ff1747fe2c49cb0f175d168396e5ac8b
Michael Eisenberg <meisenberg@easydynamics.com>
2021-12-10 17:36:34 -0500

    Support human readable names of all types
    Allow for human readable file names for all OSCAL types,
    not just for SSPs.

--- src/main/java/com/easydynamics/oscalrestservice/find-oscal-file.sh
+++ src/main/java/com/easydynamics/oscalrestservice/find-oscal-file.sh
@@ -4,12 +4,12 @@ for file in $1/*;
 do
   if [[ ${file: -5} = ".json" ]];
   then
-    contents=$(cat $file | jq --raw-output '.');
-    uuid=$(cat $file | jq --raw-output '."system-security-plan"' | jq --raw-output '."uuid"');
+    echo $oscaltype
+    contents=$(cat $file | jq --raw-output '.')
+    uuid=$(cat $file | jq --raw-output '.[]' | jq --raw-output '."uuid"')
     if [ $uuid = $2 ];
     then
-      echo $contents;
-      exit 0;
+      echo $contents
     fi
   fi
 done
