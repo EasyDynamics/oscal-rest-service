ebc1a842346c02c5e3f8404c8a8267ce6b2ded4b
Michael Eisenberg <meisenberg@easydynamics.com>
2021-12-14 12:40:47 -0500

    Avoid replacing a uuid multiple times

--- oscal-tools/replace-uuids.sh
+++ oscal-tools/replace-uuids.sh
@@ -17,7 +17,7 @@ while read -r line;
 do
 	#Extracting the uuid from the line
   old_uuid="$(echo $line | sed 's/^[ \t]*\"uuid\": //' | sed 's/[\" | ,]//g')";
-	old_uuid=${old_uuid::-1};
+	old_uuid="${old_uuid::-1}";
 
 	#Checking if we already replaced the uuid
 	array_contains=$(echo ${uuid_arr[@]} | grep -o "$old_uuid" | wc -w);
@@ -25,12 +25,13 @@ do
 	if [ $array_contains = 0 ];
 	then
 		#adding the uuid (in the current line) to the array of replaced uuids
-		uuid_arr+=($old_uuid);
+		uuid_arr+=("$old_uuid");
 
 		#auto generating a new uuid with the uuidgen command
 		auto_generated_uuid=$(uuidgen);
+		uuid_arr+=("$auto_generated_uuid");
 
 		#replacing all instances of current_uuid in file
-		sed -i "s/$old_uuid/$auto_generated_uuid/g" "$file";
+		sed -i "s/"$old_uuid"/"$auto_generated_uuid"/g" "$file";
 	fi
 done < <(cat $file | grep \"uuid\"); #We only want to read lines with uuids
