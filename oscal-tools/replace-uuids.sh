b2c7247c82b0bb99f205b75e061e0d9a3fcc9d90
Michael Eisenberg <meisenberg@easydynamics.com>
2021-12-14 12:58:55 -0500

    Update how uuids are replaced
    Replace uuids with autogenerated uuids by first
    searching the file for unique uuids, generating
    a new uuid, and placing them in their respective
    lists. Then, after the file is read, perform the
    actual replacement of uuids.

--- oscal-tools/replace-uuids.sh
+++ oscal-tools/replace-uuids.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 if [ "${1:0:11}" = "-oscalfile:" -a ${1: -5} = ".json" ]; then
 	file=${1:11};
 else
@@ -13,25 +12,26 @@ file=$(basename $file);
 #Will contain the replaced uuids to avoid replacing uuid multiple times
 uuid_arr=();
 
-while read -r line;
-do
+#Will contain the auto-generated uuids
+auto_generated_uuids=();
+
+while read -r line; do
 	#Extracting the uuid from the line
   old_uuid="$(echo $line | sed 's/^[ \t]*\"uuid\": //' | sed 's/[\" | ,]//g')";
-	old_uuid="${old_uuid::-1}";
+	old_uuid=${old_uuid::-1};
 
 	#Checking if we already replaced the uuid
 	array_contains=$(echo ${uuid_arr[@]} | grep -o "$old_uuid" | wc -w);
 
-	if [ $array_contains = 0 ];
-	then
+	if [ $array_contains = 0 ]; then
 		#adding the uuid (in the current line) to the array of replaced uuids
-		uuid_arr+=("$old_uuid");
+		uuid_arr+=($old_uuid);
 
 		#auto generating a new uuid with the uuidgen command
-		auto_generated_uuid=$(uuidgen);
-		uuid_arr+=("$auto_generated_uuid");
-
-		#replacing all instances of current_uuid in file
-		sed -i "s/"$old_uuid"/"$auto_generated_uuid"/g" "$file";
+		auto_generated_uuids+=$(uuidgen);
 	fi
 done < <(cat $file | grep \"uuid\"); #We only want to read lines with uuids
+
+for i in "${!uuid_arr[@]}"; do
+	sed -i "s/"${uuid_arr[i]}"/"${auto_generated_uuids[i]}"/g" "$file";
+done;
