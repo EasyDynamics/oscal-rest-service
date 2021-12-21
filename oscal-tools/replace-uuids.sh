a0a6ebcbf162a185430a844a03b8026601615a66
Michael Eisenberg <meisenberg@easydynamics.com>
2021-12-21 16:57:36 -0500

    Support minified json files
    Add support for minified json files and files with multiple uuids
    on a single line.
    
    Clean up script to adhere to bash scripting conventions.

--- oscal-tools/replace-uuids.sh
+++ oscal-tools/replace-uuids.sh
@@ -1,41 +1,33 @@
 #!/bin/bash
 if [ "${1: -5}" = ".json" ]; then
-	file="$1";
+	file="$1"
 else
-	echo "You must specify -oscalfile:\$file, where \$file is a json oscal file.";
-	exit 1;
+	echo "You must specify a \$FILE argument, where \$FILE is a json oscal file."
+	exit 1
 fi
 
-current_directory="$(pwd)"
-cd $(dirname $file);
-file=$(basename $file);
-contents="$(jq '.' $file | jq .)"
+#We only want to update lines containing uuid(s)
+uuids="$(sed 's/[ \t]//g' "$file" | tr ',' '\n' | grep \"uuid\")"
 
 #Will contain the replaced uuids to avoid replacing uuid multiple times
-uuid_arr=();
+uuid_array=()
 
 #Will contain the auto-generated uuids
-auto_generated_uuids=();
+auto_generated_uuids=()
 
 while read -r line; do
-	#Extracting the uuid from the line
-	old_uuid="$(echo $line | sed 's/^[ \t]*\"uuid\": //' | sed 's/[\" | ,]//g')";
-	old_uuid=${old_uuid::-1};
+	uuid=$(sed 's/^.*"uuid":"\(.*\)"/\1/g' <<< "$line") #Extracting the uuid
+  uuid_array_contains=$(echo "${uuid_array[@]}" | grep -o "$uuid" | wc -w)
 
-	#Checking if we already replaced the uuid
-	array_contains=$(echo ${uuid_arr[@]} | grep -o "$old_uuid" | wc -w);
-
-	if [ $array_contains = 0 ]; then
-		#adding the uuid (in the current line) to the array of replaced uuids
-		uuid_arr+=($old_uuid);
+  if [ "$uuid_array_contains" = 0 ]; then
+    #adding the uuid (in the current line) to the array of replaced uuids
+		uuid_array+=("$uuid")
 
 		#auto generating a new uuid with the uuidgen command
-		auto_generated_uuids+=("$(uuidgen)");
-	fi
-done < <(grep \"uuid\" <<< $contents); #We only want to read lines with uuids
+		auto_generated_uuids+=("$(uuidgen)")
+  fi
+done < <(echo "$uuids");
 
-for i in "${!uuid_arr[@]}"; do
-	sed -i "s/"${uuid_arr[i]}"/"${auto_generated_uuids[i]}"/g" "$file";
+for i in "${!uuid_array[@]}"; do
+	sed -i "s/""${uuid_array[i]}""/""${auto_generated_uuids[i]}""/g" "$file"
 done;
-
-cd "$current_directory"
