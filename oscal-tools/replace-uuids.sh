1a499957132931573dca47292c86bf57e6068444
Michael Eisenberg <meisenberg@easydynamics.com>
2021-12-22 21:57:12 -0500

    Clean up script syntax

--- oscal-tools/replace-uuids.sh
+++ oscal-tools/replace-uuids.sh
@@ -2,7 +2,7 @@
 if [ "${1: -5}" = ".json" ]; then
   file="$1"
 else
-  echo "You must specify a \$FILE argument, where \$FILE is a json oscal file."
+  echo "You must specify a \$FILE argument, where \$FILE is a JSON OSCAL file."
   exit 1
 fi
 
@@ -16,18 +16,18 @@ uuid_array=()
 auto_generated_uuids=()
 
 while read -r line; do
-  uuid=$(sed 's/^.*"uuid":"\(.*\)"/\1/g' <<< "$line") #Extracting the uuid
-  uuid_array_contains=$(echo "${uuid_array[@]}" | grep -o "$uuid" | wc -w)
+  uuid="$(sed 's/^.*"uuid":"\(.*\)"/\1/g' <<< "$line")" #Extracting the uuid
 
-  if [ "$uuid_array_contains" = 0 ]; then
-    #adding the uuid (in the current line) to the array of replaced uuids
+	#Checking if we have already found the current value of uuid in the file
+  if [[ ! $(echo "${uuid_array[@]}" | grep --quiet --only-matching "$uuid") ]]; then	
+		#adding the uuid (in the current line) to the array of replaced uuids
     uuid_array+=("$uuid")
 
     #auto generating a new uuid with the uuidgen command
     auto_generated_uuids+=("$(uuidgen)")
   fi
-done < <(echo "$uuids");
+done < <(echo "$uuids")
 
 for i in "${!uuid_array[@]}"; do
-  sed -i "s/""${uuid_array[i]}""/""${auto_generated_uuids[i]}""/g" "$file"
-done;
+  sed -i "s/${uuid_array[i]}/${auto_generated_uuids[i]}/g" "$file"
+done
