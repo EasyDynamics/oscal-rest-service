4f563ab547808611910c71ccf6a933954bf1d36c
Ray Gauss II <rgauss@rgauss.com>
2022-02-02 09:00:44 -0500

    Added (currently failing) test to reproduce behavior

---
+++ oscal-rest-service-app/src/test/java/com/easydynamics/oscalrestservice/api/CatalogControllerMisconfiguredPersistenceTests.java
@@ -0,0 +1,54 @@
+package com.easydynamics.oscalrestservice.api;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import com.easydynamics.oscal.data.example.ExampleContentConstants;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+/**
+ * Test to validate behavior of a misconfigured persistence path on the service.
+ */
+@SpringBootTest(properties = {"persistence.file.parent.path=bogus-path"})
+@AutoConfigureMockMvc
+public class CatalogControllerMisconfiguredPersistenceTests {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  protected OscalObjectType oscalObjectType;
+  protected String defaultId;
+
+  private CatalogControllerMisconfiguredPersistenceTests() {
+    this.oscalObjectType = OscalObjectType.CATALOG;
+    this.defaultId = ExampleContentConstants.CATALOG_ID_80053r5;
+  }
+
+    /**
+   * Tests if the GET Request to /<oscalTypes> will retrieve a valid list of OSCAL objects.
+   *
+   * @throws Exception the exception thrown by the REST request.
+   */
+  @Test
+  public void testMisconfiguredFindAllOscalObjects() throws Exception {
+    // With StreamingResponseBody this is async so we start the request here
+    MvcResult asyncResult = this.mockMvc.perform(get("/oscal/v1/" + oscalObjectType.restPath))
+        .andExpect(request().asyncStarted())
+        .andReturn();
+
+    // On async result check the expected empty response
+    this.mockMvc.perform(asyncDispatch(asyncResult))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType("application/json"))
+        .andExpect(jsonPath("$").isEmpty());
+  }
+
+}
