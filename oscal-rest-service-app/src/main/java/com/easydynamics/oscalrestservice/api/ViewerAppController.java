5ff46bc9bdc88d630f996c07b2fd6e8f7774dbd2
Michael Eisenberg <meisenberg@easydynamics.com>
2022-02-07 13:49:26 -0500

    Revert unnecessary changes

--- oscal-rest-service-app/src/main/java/com/easydynamics/oscalrestservice/api/ViewerAppController.java
+++ oscal-rest-service-app/src/main/java/com/easydynamics/oscalrestservice/api/ViewerAppController.java
@@ -1,14 +1,14 @@
 package com.easydynamics.oscalrestservice.api;
 
-import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
- 
+
 @Controller
-public class ErrorHandlingController implements ErrorController {
- 
-  @GetMapping("/error")
-  public String handleError() {
+public class ViewerAppController {
+
+  @GetMapping({"/system-security-plan","/catalog","/component-definition","/profile"})
+  public String getSystemSecurityPlan() {
     return "index.html";
   }
+
 }
