883d37503f1b50d6efe01e33ed70aee8717994bc
Ray Gauss II <rgauss@rgauss.com>
2022-02-08 17:57:34 -0500

    Fixed test after rebase

--- oscal-rest-service-app/src/test/java/com/easydynamics/oscalrestservice/api/CatalogControllerMisconfiguredPersistenceTests.java
+++ oscal-rest-service-app/src/test/java/com/easydynamics/oscalrestservice/api/CatalogControllerMisconfiguredPersistenceTests.java
@@ -6,7 +6,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import com.easydynamics.oscal.data.example.ExampleContentConstants;
+import com.easydynamics.oscal.data.example.ExampleContent;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -29,7 +29,7 @@ public class CatalogControllerMisconfiguredPersistenceTests {
 
   private CatalogControllerMisconfiguredPersistenceTests() {
     this.oscalObjectType = OscalObjectType.CATALOG;
-    this.defaultId = ExampleContentConstants.CATALOG_ID_80053r5;
+    this.defaultId = ExampleContent.CATALOG_80053r5.uuid;
   }
 
     /**
