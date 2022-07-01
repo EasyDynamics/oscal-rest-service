21a3f997c2f555168afc5d00b9015e7686e6a8c8
tuckerzp <ztucker@easydynamics.com>
2022-07-01 13:28:32 -0400

    Map oscal objects to correct oscal viewer asset

--- oscal-rest-service-app/src/main/java/com/easydynamics/oscalrestservice/api/ViewerAppController.java
+++ oscal-rest-service-app/src/main/java/com/easydynamics/oscalrestservice/api/ViewerAppController.java
@@ -1,14 +1,33 @@
 package com.easydynamics.oscalrestservice.api;
 
+import java.util.UUID;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.HandlerMapping;
 
 @Controller
 public class ViewerAppController {
 
-  @GetMapping({"/system-security-plan","/catalog","/component-definition","/profile"})
-  public String getSystemSecurityPlan() {
-    return "index.html";
+  /**
+   * Get the underlying asset for a request.
+   * @param request the request
+   * @return the file
+   */
+  @GetMapping({ "system-security-plan/**", "catalog/**", "component-definition/**", "profile/**" })
+  public String getOscalViewerAsset(HttpServletRequest request) {
+    String fullRequestPath =
+        (String) request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
+    String baseMapping =
+        (String) request.getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE);
+    String wildcardComponent =
+        new AntPathMatcher().extractPathWithinPattern(baseMapping, fullRequestPath);
+    try {
+      UUID.fromString(wildcardComponent);
+      return "/index.html";
+    } catch (IllegalArgumentException iae) {
+      return (wildcardComponent.isBlank() ? "/index.html" : "/" + wildcardComponent);
+    }
   }
-
 }
