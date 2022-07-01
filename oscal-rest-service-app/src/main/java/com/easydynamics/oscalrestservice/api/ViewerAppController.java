acacdb7a54ce16e917b8f66478b24a35942b0396
tuckerzp <ztucker@easydynamics.com>
2022-07-01 13:50:03 -0400

    Clarify code with javadoc comments

--- oscal-rest-service-app/src/main/java/com/easydynamics/oscalrestservice/api/ViewerAppController.java
+++ oscal-rest-service-app/src/main/java/com/easydynamics/oscalrestservice/api/ViewerAppController.java
@@ -11,9 +11,10 @@ import org.springframework.web.servlet.HandlerMapping;
 public class ViewerAppController {
 
   /**
-   * Get the underlying asset for a request.
-   * @param request the request
-   * @return the file
+   * Get the underlying asset map given a request url.
+   *
+   * @param request - the request given by the user 
+   * @return the file within the service that the request maps to
    */
   @GetMapping({ "system-security-plan/**", "catalog/**", "component-definition/**", "profile/**" })
   public String getOscalViewerAsset(HttpServletRequest request) {
@@ -23,6 +24,25 @@ public class ViewerAppController {
         (String) request.getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE);
     String wildcardComponent =
         new AntPathMatcher().extractPathWithinPattern(baseMapping, fullRequestPath);
+   
+    // This does a few things that are not exactly obvious why they are
+    // necessary to be done. 
+    //
+    // UUID.fromString throws an exception when attempting to parse an invalud
+    // UUID. So upon successfully parsing a uuid from the wildcard, /index.html
+    // is returned. 
+    //
+    // When a uuid is not passed, there are two options to deal with. If a
+    // "/" was added on the end with no actual wildcard, we just return
+    // /index.html. In the case where something other than a uuid is in the
+    // wildcard, we return /index.html/{wildCard} and let that get mapped if
+    // needed.  
+    //
+    // All files returned must be prepended with "/". This is due to some
+    // wonkiness with things being returned as absolute paths.
+    //
+    // Another very important thing to note is that if we try to add anything 
+    // after the uuid, this will fail. 
     try {
       UUID.fromString(wildcardComponent);
       return "/index.html";
